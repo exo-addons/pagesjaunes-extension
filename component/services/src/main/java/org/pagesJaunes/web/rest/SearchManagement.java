90bda707d1e15dffe6a528e0d70dcf9c93721af6
ayoub zayati <ayoub.ezzayati@exoplatform.com>
2013-12-10 13:27:00 +0100

    Implementer le service searchManagement Implementer la gadget de recherche

--- component/services/src/main/java/org/pagesJaunes/web/rest/SearchManagement.java
+++ component/services/src/main/java/org/pagesJaunes/web/rest/SearchManagement.java
@@ -1,34 +1,61 @@
 package org.pagesJaunes.web.rest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Iterator;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 
 @Path("/searchManagement/")
 public class SearchManagement implements ResourceContainer {
 	
+	private static final Log LOGGER = ExoLogger.getLogger("SearchManagement.class");
+	
     @GET
     @Path("getSearchResult/{quoiqui}/{ou}")
-    public String  getSearchResult(@PathParam("quoiqui") String quoiqui, @PathParam("ou") String ou) {
+    @Produces("application/json")
+    public Response  getSearchResult(@PathParam("quoiqui") String quoiqui, @PathParam("ou") String ou) {
     	String url = "http://api.apipagesjaunes.fr/v2/pro/find.json?max=4&what="+ quoiqui + "&where=" + ou + "&app_id=e74d895a&app_key=5050ac249e48f00795c39a06a8af7235";
-    	return getData(url);
+    	Response response;
+    	String data = getData(url);
+		try {
+	    	if (data != null) {
+	            if (data.equalsIgnoreCase("null")) {
+	            	response = Response.status(200).entity(data).build();
+	            }
+	            else {
+	            	JSONObject dataJsonObject = new JSONObject(data);
+	                response = Response.status(200).entity(dataJsonObject.toString()).build();
+	            }
+	        }
+	        else {
+	            response = Response.status(404).build();
+	        }
+		} catch (Throwable throwable) {
+            LOGGER.error(throwable.toString());
+            response = Response.status(500).build();
+        }
+    	return response;
     }
 	
     private String getData(String url) {
     	HttpURLConnection connection = null;
         StringBuilder searchResults = null;
         URL serverAddress = null;
+        BufferedReader bufferedReadere  = null;
+        String line = null;
         try {
         	serverAddress = new URL(url);
             //set up out communications stuff
@@ -39,55 +66,14 @@ public class SearchManagement implements ResourceContainer {
             connection.setDoOutput(true);
             connection.setReadTimeout(10000);
             connection.connect();
-            //create ObjectMapper instance
-            ObjectMapper objectMapper = new ObjectMapper();
-             
-            //read JSON like DOM Parser
-            JsonNode rootNode = objectMapper.readTree(connection.getInputStream());
-            JsonNode contextNode = rootNode.path("context");
-            JsonNode searchNode = contextNode.path("search");
-            JsonNode whatNode = searchNode.path("what");
-            JsonNode whereNode = searchNode.path("where");
-            JsonNode resultsNode = contextNode.path("results");
-            JsonNode totalListingNode = resultsNode.path("total_listing");
-            searchResults = new StringBuilder("<b>" + whatNode.asText() + " à " + whereNode.asText() + " : " + totalListingNode + " résultats" + "</b></br></br>");
-            
-            JsonNode searchResultsNode = rootNode.path("search_results");
-            JsonNode listingsNode = searchResultsNode.path("listings");
-            Iterator<JsonNode> listingsNodeElements = listingsNode.elements();
-            JsonNode listingNode;
-            JsonNode merchantNameNode;
-            JsonNode descriptionNode;
-            JsonNode thumbnailUrlNode;
-            JsonNode inscriptionsNode;
-            JsonNode inscriptionNode;
-            JsonNode adressStreetNode = null;
-            while(listingsNodeElements.hasNext()){
-            	listingNode = listingsNodeElements.next();
-                thumbnailUrlNode = listingNode.path("thumbnail_url");
-                if (thumbnailUrlNode != null){
-                	searchResults.append("<img src='" + thumbnailUrlNode.asText() + "'/>");
-                }
-                merchantNameNode = listingNode.path("merchant_name");
-                if (merchantNameNode != null){
-                	searchResults.append("<b style='text-decoration:underline;font-size:small;'>" + merchantNameNode.asText() + "</b></br>");
-                }
-                inscriptionsNode = listingNode.path("inscriptions");
-                Iterator<JsonNode> inscriptionsNodeElements = inscriptionsNode.elements();
-                while(inscriptionsNodeElements.hasNext()){
-                	inscriptionNode = inscriptionsNodeElements.next();
-                	adressStreetNode = inscriptionNode.path("adress_street");
-                	break;
-                }	
-                if (adressStreetNode != null){
-                	searchResults.append("<span style='color:grey;font-size:small;'>" + adressStreetNode.asText() + "</span></br>");
-                }
-                descriptionNode = listingNode.path("description");
-                if (adressStreetNode != null){
-                	searchResults.append("<span style='font-size:small;'>" + descriptionNode.asText() + "</span></br>");
-                }
-                searchResults.append("________________________________________________________________________</br>");
+            //read the result from the server
+            bufferedReadere  = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+            searchResults = new StringBuilder();
+            while ((line = bufferedReadere.readLine()) != null)
+            {
+            	searchResults.append(line + '\n');
             }
+            
             return searchResults.toString();
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -100,240 +86,8 @@ public class SearchManagement implements ResourceContainer {
             connection.disconnect();
             searchResults = null;
             connection = null;
+            bufferedReadere = null;
         }
         return null;
     }
-    
-//    public static class JsonSearchResult {
-//        Context context;
-//        SearchResults search_results;
-//        
-//        public Context getContext() {
-//        	return context;
-//        }
-//        
-//        public SearchResults getSearchResults() {
-//            return search_results;
-//        }
-//    }
-//    
-//    public static class Context {
-//        Search search;
-//        Results results;
-//        Pages pages;
-//        
-//        public Search getSearch() {
-//        	return search;
-//        }
-//        
-//        public Results getResults() {
-//            return results;
-//        }
-//        
-//        public Pages getPages() {
-//            return pages;
-//        }
-//    }
-//    
-//    public static class Search {
-//        String what;
-//        String where;
-//        
-//        public String getWhat() {
-//        	return what;
-//        }
-//        
-//        public String getWhere() {
-//            return where;
-//        }
-//    }
-//    
-//    public static class Results {
-//        String first_listing;
-//        String last_listing;
-//        String total_listing;
-//        
-//        public String getFirstListing() {
-//        	return first_listing;
-//        }
-//        
-//        public String getLastListing() {
-//            return last_listing;
-//        }
-//        
-//        public String getTotalListing() {
-//            return total_listing;
-//        }
-//    }
-//    
-//    public static class Pages {
-//        String current_page;
-//        String page_count;
-//        String listings_per_page;
-//        String current_page_url;
-//        String next_page_url;
-//
-//        public String getCurrentPage() {
-//        	return current_page;
-//        }
-//        
-//        public String getPageCount() {
-//            return page_count;
-//        }
-//        
-//        public String getListingsPerPage() {
-//            return listings_per_page;
-//        }
-//        
-//        public String getCurrentPageUrl() {
-//            return current_page_url;
-//        }
-//        
-//        public String getNextPageUrl() {
-//            return next_page_url;
-//        }
-//    }
-//    
-//    public static class SearchResults {
-//        List<Listings> listings;
-//        
-//        public List<Listings> getListings() {
-//        	return listings;
-//        }
-//    }
-//    
-//    public static class Listings {
-//        String listing_id;
-//        String merchant_id;
-//        String merchat_name;
-//        String thumbnail_url;
-//        String description;
-//        List<String> website_urls;
-//        boolean eco_label;
-//        List<String> categories;
-//        Inscriptions inscriptions;
-//        
-//        public String getListingId() {
-//          return listing_id;
-//        }
-//        
-//        public String getMerchantId() {
-//            return merchant_id;
-//        }
-//        
-//        public String getMerchantName() {
-//            return merchat_name;
-//        }
-//        
-//        public String getThumbnailUrl() {
-//            return thumbnail_url;
-//        }
-//        
-//        public String getDescription() {
-//            return description;
-//        }
-//        
-//        public List<String> getWebsiteUrls() {
-//            return website_urls;
-//        }
-//        
-//        public boolean getEcoLabel() {
-//            return eco_label;
-//        }
-//        
-//        public List<String> getCategories() {
-//            return categories;
-//        }
-//        
-//        public Inscriptions getInscriptions() {
-//            return inscriptions;
-//        }
-//    }
-//    
-//    public static class Inscriptions {
-//        String inscription_id;
-//        String pro_id;
-//        String label;
-//        String adress_street;
-//        String adress_zipcode;
-//        String adress_city;
-//        String latitude;
-//        String longitude;
-//        Reviews reviews;
-//        ContactInfo contact_info1;
-//        ContactInfo contact_info2;
-//        
-//		public String getInscriptionId() {
-//			return inscription_id;
-//		}
-//		
-//		public String getProId() {
-//			return pro_id;
-//		}
-//		
-//		public String getLabel() {
-//			return label;
-//		}
-//		
-//		public String getAdressStreet() {
-//			return adress_street;
-//		}
-//		public String getAdressZipcode() {
-//			return adress_zipcode;
-//		}
-//		public String getAdressCity() {
-//			return adress_city;
-//		}
-//		public String getLatitude() {
-//			return latitude;
-//		}
-//		public String getLongitude() {
-//			return longitude;
-//		}
-//		public Reviews getReviews() {
-//			return reviews;
-//		}
-//		public ContactInfo getContactInfo1() {
-//			return contact_info1;
-//		}
-//		public ContactInfo getContactInfo2() {
-//			return contact_info2;
-//		}
-//    }
-//    
-//    public static class Reviews {
-//        String total_reviews;
-//        String overall_review_rating;
-//        
-//        public String getTotalReviews() {
-//        	return total_reviews;
-//        }
-//        
-//        public String getOverallReviewRating() {
-//        	return overall_review_rating;
-//        }
-//    }
-//    
-//    public static class ContactInfo {
-//        String contact_type;
-//        String contact_value;
-//        String phone_number_pricing;
-//        boolean no_direct_marketing;
-//        
-//        public String getContactType() {
-//            return contact_type;
-//        }
-//          
-//        public String getContactValue() {
-//            return contact_value;
-//        }
-//        
-//        public String getPhoneNumberPricing() {
-//            return phone_number_pricing;
-//        }
-//        
-//        public boolean getNoDirectMarketing() {
-//            return no_direct_marketing;
-//        }
-//    }
 }
--- war/src/main/webapp/gadgets/PagesJaunes/skin/PagesJaunesRecherche.js
+++ war/src/main/webapp/gadgets/PagesJaunes/skin/PagesJaunesRecherche.js
@@ -1,22 +1,51 @@
 function updateSearchResults() {
 	var quoiqui = $("#quoiqui").val();
 	var ou = $("#ou").val();
-	$.ajax
-    ({
+	$.ajax ({
         cache: true,
         url: "http://localhost:8080/rest/searchManagement/getSearchResult/" + quoiqui + "/" + ou,
     })
     .fail
     (
-        function () 
-        {
+        function () {
         }
     )
     .done
     (
-        function (result) 
-        {
-             $("div#searchResults").html( result );
+        function (result) {
+        	var totalListing = result["context"]["results"]["total_listing"];
+            var html = "<b>" + quoiqui + " à " + ou + " : " + totalListing + " résultats" + "</b></br></br>";
+            var listings = result["search_results"]["listings"];
+            var thumbnailUrl;
+            var merchantName;
+            var inscriptions;
+            var adressStreet;
+            var description;
+            for (i = 0; i < listings.length; i++){
+            	thumbnailUrl = listings[i]["thumbnail_url"];
+            	if (thumbnailUrl != null){
+            		html += "<img src='" + thumbnailUrl + "'/>";
+            	}
+            	merchantName = listings[i]["merchant_name"];
+            	if (merchantName != null){
+            		html += "<b style='text-decoration:underline;font-size:small;'>" + merchantName + "</b></br>";
+            	}
+            	inscriptions = listings[i]["inscriptions"];
+            	for (j = 0; j < inscriptions.length; i++){
+            		adressStreet = inscriptions[j]["adress_street"];
+            		break;
+            	}
+            	if (adressStreet != null) {
+            		html += "<span style='color:grey;font-size:small;'>" + adressStreet + "</span></br>";
+            	}
+            	description = listings[i]["description"];
+            	if (description != null){
+            		html += "<span style='font-size:small;'>" + description + "</span></br>";
+            	}
+            	html += "________________________________________________________________________</br>";
+            }
+            
+        	$("div#searchResults").html(html);
         }
     );
 }
