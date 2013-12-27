17eda658d0303b552ecec8c6dfac7bc931e133b9
ayoub zayati <ayoub.ezzayati@exoplatform.com>
2013-12-27 17:53:43 +0100

    Ajouter les statistiques "Addon install"
    Signed-off-by: ayoub zayati <ayoub.ezzayati@exoplatform.com>

--- services/src/main/java/org/pagesJaunes/web/rest/SearchManagement.java
+++ services/src/main/java/org/pagesJaunes/web/rest/SearchManagement.java
@@ -26,6 +26,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -43,6 +46,9 @@ import org.exoplatform.forum.service.Forum;
 import org.exoplatform.forum.service.ForumService;
 import org.exoplatform.forum.service.MessageBuilder;
 import org.exoplatform.forum.service.Topic;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.config.RepositoryConfigurationException;
+import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
@@ -60,18 +66,23 @@ import org.json.JSONObject;
 
 @Path("/searchManagement/")
 public class SearchManagement implements ResourceContainer {
-	private static final Log LOGGER = ExoLogger.getLogger("SearchManagement.class");
-	private static final String ENTREPRISE_ADDRESS = "entreprise.address";
+	private static final String APP_ID = "app.id";
+	private static final String APP_KEY = "app.key";
 	private static final String CATEGORY_ID = "category.id";
 	private static final String CATEGORY_NAME = "category.name";
 	private static final String CATEGORY_OWNER = "category.owner";
 	private static final String CATEGORY_DESCRIPTION = "category.description";
+	private static final String ENTREPRISE_ADDRESS = "entreprise.address";
 	private static final String FORUM_ID = "forum.id";
 	private static final String FORUM_NAME = "forum.name";
 	private static final String FORUM_OWNER = "forum.owner";
 	private static final String FORUM_DESCRIPTION = "forum.description";
-	private static final String APP_ID = "app.id";
-	private static final String APP_KEY = "app.key";
+	private static final String INSTALL_STAT_NODE = "installStat";
+	private static final String INSTALL_STAT_NODETYPE = "exo:InstallStat";
+	private static final Log LOGGER = ExoLogger.getLogger("SearchManagement.class");
+	private static final String REPOSITORY = "repository";
+	private static final String SYSTEM_WORKSPACE = "system";
+	
 	
 	private String getData(String url) {
     	HttpURLConnection connection = null;
@@ -92,13 +103,13 @@ public class SearchManagement implements ResourceContainer {
             //read the result from the server
             bufferedReadere  = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
             searchResults = new StringBuilder();
-            while ((line = bufferedReadere.readLine()) != null){
+            while ((line = bufferedReadere.readLine()) != null) {
             	searchResults.append(line + '\n');
             }
             return searchResults.toString();
-        } catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         finally {
@@ -111,6 +122,31 @@ public class SearchManagement implements ResourceContainer {
         return null;
     }
 	
+	@GET
+    @Path("addInstallStatNode")
+    public String addInstallStatNode() {
+		try {
+			RepositoryService repositoryService = (RepositoryService) ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(RepositoryService.class);
+			ManageableRepository manageableRepository;
+			manageableRepository = repositoryService.getRepository(REPOSITORY);
+			Session session = manageableRepository.getSystemSession(SYSTEM_WORKSPACE);
+			Node rootNode = (Node)session.getRootNode();
+			if (rootNode.hasNode("installStat")) {
+				return "false";
+			}
+			else {
+				rootNode.addNode(INSTALL_STAT_NODE, INSTALL_STAT_NODETYPE);
+				session.save();
+				return "true";
+			}
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+		} catch (RepositoryConfigurationException e) {
+			e.printStackTrace();
+		}
+    	return null;
+    }
+	
 	@POST
     @Path("addTopic")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -120,7 +156,7 @@ public class SearchManagement implements ResourceContainer {
 	    	String categoryId = PropertyManager.getProperty(CATEGORY_ID);
 	    	String categoryName = PropertyManager.getProperty(CATEGORY_NAME);
 	    	Category category = forumService.getCategory(categoryId);
-	    	if (category ==  null){
+	    	if (category ==  null) {
 	    		category = new Category(categoryId);
 		    	category.setCategoryName(categoryName);
 		    	category.setOwner(PropertyManager.getProperty(CATEGORY_OWNER));
@@ -130,7 +166,7 @@ public class SearchManagement implements ResourceContainer {
 	    	String forumId = PropertyManager.getProperty(FORUM_ID);
 	    	String forumName = PropertyManager.getProperty(FORUM_NAME);
 	    	Forum forum = forumService.getForum(categoryId, forumId);
-	    	if (forum ==  null){
+	    	if (forum ==  null) {
 	    		forum = new Forum();
 	    		forum.setId(forumId);
 	    		forum.setForumName(forumName);
@@ -144,7 +180,7 @@ public class SearchManagement implements ResourceContainer {
 	    	topic.setDescription(forumTopic.getMessage());
 	    	topic.setIcon("uiIconForumTopic uiIconForumLightGray");
 	    	forumService.saveTopic(categoryId, forumId, topic, true, false, new MessageBuilder());
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return Response.ok().entity("OK").build();
@@ -156,7 +192,7 @@ public class SearchManagement implements ResourceContainer {
     public Response getSearchResults(@PathParam("serviceUriParams") String serviceUriParams) {
 		Response response;
 		try {
-			if (serviceUriParams.split("&where=&").length > 1){
+			if (serviceUriParams.split("&where=&").length > 1) {
 				serviceUriParams = serviceUriParams.split("&where=&")[0] + "&where=" + PropertyManager.getProperty(ENTREPRISE_ADDRESS) + "&" + serviceUriParams.split("&where=&")[1];
 			}
 			String whereAttribute = serviceUriParams.split("&where=")[1].split("&")[0];
@@ -166,19 +202,19 @@ public class SearchManagement implements ResourceContainer {
 			serviceUriParams += "&app_id=" + PropertyManager.getProperty(APP_ID) + "&app_key=" +  PropertyManager.getProperty(APP_KEY);
 	    	String url = "http://api.apipagesjaunes.fr/v2/pro/find.json?" + serviceUriParams;
 	    	String data = getData(url);
-	    	if (data != null){
-	            if (data.equalsIgnoreCase("null")){
+	    	if (data != null) {
+	            if (data.equalsIgnoreCase("null")) {
 	            	response = Response.status(200).entity(data).build();
 	            }
-	            else{
+	            else {
 	            	JSONObject dataJsonObject = new JSONObject(data);
 	                response = Response.status(200).entity(dataJsonObject.toString()).build();
 	            }
 	        }
-	        else{
+	        else {
 	            response = Response.status(404).build();
 	        }
-		} catch (Throwable throwable){
+		} catch (Throwable throwable) {
             LOGGER.error(throwable.toString());
             response = Response.status(500).build();
         }
@@ -191,13 +227,13 @@ public class SearchManagement implements ResourceContainer {
     public Response shareSearchResult(@Context HttpServletRequest request, AsMessage asMessage) {
 		String espace = asMessage.getEspace();
 		String type = asMessage.getType();
-		if (type.equals("space") && espace != null ){
+		if (type.equals("space") && espace != null ) {
 			Identity spaceIdentity = Utils.getIdentityManager().getOrCreateIdentity(SpaceIdentityProvider.NAME, espace, false);
 		    ExoSocialActivity activity = new ExoSocialActivityImpl(Utils.getUserIdentity(request.getRemoteUser(), false).getId(), SpaceService.SPACES_APP_ID, asMessage.getPostedMessage(), null);
 		    activity.setType(UIDefaultActivity.ACTIVITY_TYPE);
 		    Utils.getActivityManager().saveActivityNoReturn(spaceIdentity, activity);
 		}
-		else{
+		else {
 			Identity ownerIdentity = Utils.getIdentityManager().getOrCreateIdentity(OrganizationIdentityProvider.NAME, request.getRemoteUser(), false);
 		    ExoSocialActivity activity = new ExoSocialActivityImpl(Utils.getUserIdentity(request.getRemoteUser(), false).getId(), PeopleService.PEOPLE_APP_ID, asMessage.getPostedMessage(), null);
 		    activity.setType(UIDefaultActivity.ACTIVITY_TYPE);
--- war/src/main/webapp/gadgets/PagesJaunes/skin/PagesJaunesRecherche.js
+++ war/src/main/webapp/gadgets/PagesJaunes/skin/PagesJaunesRecherche.js
@@ -21,19 +21,32 @@ $(document).ready(function() {
 	var hpStatHtml = "<img alt='' src='http://logc258.at.pagesjaunes.fr/hit.xiti?s=540649&p=HP_PJ&rn=" + hpRandomNumber + "'>";
 	$("#searchForm").append(hpStatHtml);
 	var xtkey=false;
-	if(document.addEventListener){
-		document.addEventListener('keydown',function(){xtkey=true},false);
-		document.addEventListener('keyup',function(){xtkey=false},false);
+	if(document.addEventListener) {
+		document.addEventListener('keydown',function() {xtkey=true},false);
+		document.addEventListener('keyup',function() {xtkey=false},false);
 	}
-	else if(document.attachEvent){
-		document.attachEvent('onkeydown',function(){xtkey=true});
-		document.attachEvent('onkeyup',function(){xtkey=false});
+	else if(document.attachEvent) {
+		document.attachEvent('onkeydown',function() {xtkey=true});
+		document.attachEvent('onkeyup',function() {xtkey=false});
 	}
+	$.ajax ({
+        cache: true,
+        url: "/rest/searchManagement/addInstallStatNode",
+    })
+    .fail (
+    )
+    .done (
+    	function(result) {
+    		if (result == "true") {
+    			alert("PJ Addon installed");
+    		}
+    	}
+    );
 });
 
 $(document).keypress(function(event) {
 	var keycode = (event.keyCode ? event.keyCode : event.which);
-	if(keycode == "13"){
+	if(keycode == "13") {
 		updateSearchResults();
 		return xt_click(this,'C','','Trouver::Trouver_HP','A');
 	}
@@ -88,7 +101,7 @@ function shareSearchResult(merchantName, merchantUrl, i) {
 	html += "<button type='button' class='b-close'>Annuler</button></div></fieldset></form>";
 	$("#inline_partage" + i).html(html);
 	$("#type" + i).change(function() {
-		if ($("#type" + i).val() != "user"){
+		if ($("#type" + i).val() != "user") {
 			$.ajax ({
 		        cache: true,
 		        url: "/rest/portal/social/spaces/mySpaces/show.json",
@@ -101,9 +114,9 @@ function shareSearchResult(merchantName, merchantUrl, i) {
 		        function(result) {
 		        	var spaces = result["spaces"];
 		        	var html = "";
-		        	if (spaces !== undefined && spaces.length > 0){
+		        	if (spaces !== undefined && spaces.length > 0) {
 		        		$("#bloc_espace" + i).show();
-		        		for (j = 0; j < spaces.length; j++){
+		        		for (j = 0; j < spaces.length; j++) {
 		        			html += "<option value='" + spaces[j]["name"] + "'>" + spaces[j]["name"]  + "</option>"
 		        		}
 		        		$("#espace" + i).html(html);
@@ -176,7 +189,7 @@ function updateSearchResults(serviceUriParams, proximity) {
         	where = where == "" ? result["context"]["search"]["where"] : where;
         	$("#quoiqui").val(what);
         	$("#ou").val(where);
-        	if (listings !== undefined){
+        	if (listings !== undefined) {
 	        	var totalListing = result["context"]["results"]["total_listing"];
 	            html = "<h2 style='text-align: center;font-weight:bold'>" + what + " à " + where + " : " + totalListing + " résultats" + "</h2>";
 	            var thumbnailUrl;
@@ -189,14 +202,14 @@ function updateSearchResults(serviceUriParams, proximity) {
 	            var merchantUrl;
 	            var description;
 	            var currentPage = result["context"]["pages"]["current_page"];
-	            for (i = 0; i < listings.length; i++){
+	            for (i = 0; i < listings.length; i++) {
 	            	thumbnailUrl = listings[i]["thumbnail_url"];
-	            	if (thumbnailUrl != null){
+	            	if (thumbnailUrl != null) {
 	            		html += "<img src='" + thumbnailUrl + "'/>";
 	            	}
 	            	merchantName = listings[i]["merchant_name"];
 	            	inscriptions = listings[i]["inscriptions"];
-	            	for (j = 0; j < inscriptions.length; j++){
+	            	for (j = 0; j < inscriptions.length; j++) {
 	            		adressStreet = inscriptions[j]["adress_street"];
 	            		distance = inscriptions[j]["distance"];
 	            		contactInfo = inscriptions[j]["contact_info"];
@@ -204,22 +217,22 @@ function updateSearchResults(serviceUriParams, proximity) {
 	            		merchantUrl = inscriptions[j]["urls"]["merchant_url"];
 	            		break;
 	            	}
-	            	if (merchantName != null){
+	            	if (merchantName != null) {
 	            		html += "<b style='text-decoration:underline;font-size:small;'>" + merchantName + "</b>";
 	            	}
-	            	if (distance != null){
+	            	if (distance != null) {
 	            		html += "<span> à " + distance + "m</span>";
 	            	}
-	            	if (adressStreet != null){
+	            	if (adressStreet != null) {
 	            		html += "<br/><span style='color:grey;font-size:small;'>" + adressStreet + "</span><br/>";
 	            	}
 	            	description = listings[i]["description"];
-	            	if (description != null){
+	            	if (description != null) {
 	            		html += "<span style='font-size:small;'>" + description + "</span>";
 	            	}
-	            	if (contactInfo != null){
+	            	if (contactInfo != null) {
 	            		var contactInfoHtml = "";
-	            		for (k = 0; k < contactInfo.length; k++){
+	            		for (k = 0; k < contactInfo.length; k++) {
 	            			contactInfoHtml += "<b>" + contactInfo[k]["contact_type"] + "</b>: " + contactInfo[k]["contact_value"] + "<br/>";
             			}
 	            		html += "<span id='displayNumber" + currentPage + i + "'><button onClick='displayNumber(\"" + contactInfoHtml + "\",\"" + currentPage + i + "\");return xt_click(this,\"C\",\"\",\"BI::contact::afficher_numero\",\"A\");'>Afficher le numéro</button></span><br/>";
@@ -227,24 +240,24 @@ function updateSearchResults(serviceUriParams, proximity) {
 	            	html += "<a onClick='return xt_click(this,\"C\",\"\",\"BI::contact::itineraire\",\"A\");' href='" + itineraryUrl + "' target='_blank' style='font-weight:bold;text-decoration:underline;font-size:small;color:blue'>Itinéraire</a>";
 	            	html += "<a href='#' onClick='shareSearchResult(\"" + escape(merchantName)  + "\",\"" + escape(merchantUrl) + "\",\"" + currentPage + i + "\");return xt_click(this,\"C\",\"\",\"BI::partager\",\"A\");' style='margin-left:200px;font-weight:bold;text-decoration:underline;font-size:small;color:blue'>Partager</a>";
 	            	html += "<a href='#' onClick='addTopic(\"" + escape(merchantName)  + "\",\"" + escape(merchantUrl) + "\",\"" + currentPage + i + "\");return xt_click(this,\"C\",\"\",\"BI::discussion\",\"A\");' style='margin-left: 300px;font-weight:bold;text-decoration:underline;font-size:small;color:blue'>Ouvrir une discussion</a><br/>";
-	            	if ($("#inline_partage" + currentPage + i).length == 0){
+	            	if ($("#inline_partage" + currentPage + i).length == 0) {
 	            		html += "<div style='display:none' id='inline_partage" + currentPage + i + "'></div>";
 	            	}
-	            	if ($("#inline_discussion" + currentPage + i).length == 0){
+	            	if ($("#inline_discussion" + currentPage + i).length == 0) {
 	            		html += "<div style='display:none' id='inline_discussion" + currentPage + i + "'></div>";
 	            	}
 	            	html += "________________________________________________________________________<br/>";
 	            }
 	            var prevPageUrl = result["context"]["pages"]["prev_page_url"];
 	            var nextPageUrl = result["context"]["pages"]["next_page_url"];
-	            if (prevPageUrl != null){
+	            if (prevPageUrl != null) {
 		            var whereAttribute = prevPageUrl.split("&where=")[1].split("&")[0];
 		            prevPageUrl = prevPageUrl.split(whereAttribute)[0] + where + prevPageUrl.split(whereAttribute)[1];
 		        	var whatAttribute = prevPageUrl.split("&what=")[1].split("&")[0];
 		        	prevPageUrl = prevPageUrl.split(whatAttribute)[0] + what + prevPageUrl.split(whatAttribute)[1];
 	            	html += "<a style='font-weight:bold;' href='#' onClick='updateSearchResults(\"" + prevPageUrl.split('?')[1] + "\")'>Page précédante</a>";
 	            }
-	            if (nextPageUrl != null){
+	            if (nextPageUrl != null) {
 	            	var whereAttribute = nextPageUrl.split("&where=")[1].split("&")[0];
 	            	nextPageUrl = nextPageUrl.split(whereAttribute)[0] + where + nextPageUrl.split(whereAttribute)[1];
 			        var whatAttribute = nextPageUrl.split("&what=")[1].split("&")[0];
@@ -261,14 +274,14 @@ function updateSearchResults(serviceUriParams, proximity) {
 function xt_click(obj,type,section,page,x1,x2,x3,x4,x5) {             
 	var xtImg=new Image(),xtDate=new Date(),xtScr=window.screen,xtNav=window.navigator,xtObj=null;         
 	var xtSrc='http://logc258.at.pagesjaunes.fr/hit.xiti?s=540649&s2='+section+'&p='+page+((type=='F')?'':(type=='M')?'&a='+x1+'&m1='+x2+'&m2='+x3+'&m3='+x4+'&m4='+x5:'&clic='+x1)+'&hl='+xtDate.getHours()+'x'+xtDate.getMinutes()+'x'+xtDate.getSeconds();               
-	if(parseFloat(xtNav.appVersion)>=4){
+	if(parseFloat(xtNav.appVersion)>=4) {
 		xtSrc+='&r='+xtScr.width+'x'+xtScr.height+'x'+xtScr.pixelDepth+'x'+xtScr.colorDepth;
 	}
-	xtImg.src=xtSrc;xtImg.onload=function(){xtImg.onload=null;};    
-	if(obj.nodeName!='A'){
+	xtImg.src=xtSrc;xtImg.onload=function() {xtImg.onload=null;};    
+	if(obj.nodeName!='A') {
 		var xelp=obj.parentNode;
-		while(xelp){
-			if(xelp.nodeName=='A'){
+		while(xelp) {
+			if(xelp.nodeName=='A') {
 				xtObj=xelp;break;
 			}
 		xelp=xelp.parentNode;
@@ -277,37 +290,37 @@ function xt_click(obj,type,section,page,x1,x2,x3,x4,x5) {
 	else {
 		xtObj=obj;
 	}             
-	if(xtObj){
+	if(xtObj) {
 		xtObj.target=xtObj.target||'_self';
-		if(x2&&(type=='C')){
+		if(x2&&(type=='C')) {
 			xtObj.href=x2;
-			if(x3){
+			if(x3) {
 				xtObj.target='_blank';
 			}
-			else{
+			else {
 				xtObj.target='_self';
 			}
 		}
-		if(!xtkey){
-			if(xtObj.target.toLowerCase()=='_self'){
+		if(!xtkey) {
+			if(xtObj.target.toLowerCase()=='_self') {
 				setTimeout('self.location.href="'+xtObj.href+'"',500);
 				return false;
 			} 
-			else if(xtObj.target.toLowerCase()=='_top'){
+			else if(xtObj.target.toLowerCase()=='_top') {
 				setTimeout('top.location.href="'+xtObj.href+'"',500);
 				return false;
 			} 
-			else if(xtObj.target.toLowerCase()=='_parent'){
+			else if(xtObj.target.toLowerCase()=='_parent') {
 				setTimeout('parent.location.href="'+xtObj.href+'"',500);
 				return false;
 			}
 		}
 	} 
-	else if(x2&&(type=='C')){
-		if(x3){
+	else if(x2&&(type=='C')) {
+		if(x3) {
 			setTimeout('(window.open("'+x2+'","_blank")).focus();',500);
 		} 
-		else{
+		else {
 			setTimeout('self.location.href="'+x2+'"',500);
 		}
 	}
